b578e62ac3f18a42420b1f019e4e6409fcc4c404
theNenufar <nenufarmarinidias@gmail.com>
2026-04-16 19:35:29 -0300

    feat: added all cards images + added card grid to cards.tsx

--- app/(tabs)/cartas.tsx
+++ app/(tabs)/cartas.tsx
@@ -1,13 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import cartas from "../../src/data/cartas.json";
+import { imagensCartas } from "../../src/utils/images";
 
 export default function Cartas() {
     return (
-        <View>
-            <Text>Cartas</Text>
-            {cartas.map((carta) => (
-                <Text key={carta.id}>{carta.name}</Text>
-            ))}
-        </View>
+        <FlatList
+            data={cartas}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={2}
+            renderItem={({ item }) => (
+                <View style={styles.card}>
+                    <Image source={imagensCartas[item.id]} style={styles.image} />
+                    <Text style={styles.nome}>{item.name}</Text>
+                </View>
+            )}
+        />
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        flex: 1,
+        margin: 10,
+        alignItems: "center"
+    },
+
+    image: {
+        width: 130,
+        height: 210,
+        borderRadius: 4
+    },
+
+    nome: {
+        marginTop: 5,
+        textAlign: "center"
+    }
+});
